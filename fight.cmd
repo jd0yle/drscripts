a10ff5dd4f6e7d0007c4aadcdfe942852d883b1c
Justin Doyle <justin@jmdoyle.com>
2021-04-19 21:05:07 -0400

    Added missing stance trigger to fight

--- fight.cmd
+++ fight.cmd
@@ -124,6 +124,9 @@ action var useHunt 0 when ^You find yourself unable to hunt in this area.
 
 action var noAmmo 1 when ^You don't have the proper ammunition readily available
 
+put #trigger {^You are now set to use your (\S+) stance} {#var lastStanceGametime $gametime;#var stance $1} {stance}
+put #trigger {e/$stance/} {#statusbar 7 Stance: $stance} {stance}
+
 ###############################
 ###      init
 ###############################
