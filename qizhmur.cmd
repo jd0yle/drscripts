87143a99db8d9057c4bd7a108acd09330c89d173
Justin Doyle <justin@jmdoyle.com>
2021-04-30 02:02:39 -0400

    Fixed TM in fight

--- qizhmur.cmd
+++ qizhmur.cmd
@@ -3,7 +3,7 @@ include libmaster.cmd
 #put .var_Qizhmur
 #waitforre ^CHARVARS DONE
 
-var expectedNumBolts twenty
+var expectedNumBolts eighteen
 
 action goto logout when eval $health < 50
 action goto logout when eval $dead = 1
