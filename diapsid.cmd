fff9cb4e2ec1fbbb04ed07468ae2f91095bc134d
snmurphy <snmurphy95@gmail.com>
2021-05-20 20:34:06 -0400

    Variable fix and fix for bolt making.

--- diapsid.cmd
+++ diapsid.cmd
@@ -137,7 +137,7 @@ offerPrize:
 
 
 requestCoinFix:
-    put ooc $winner [DR Discord Giveaways] Please use AVOID COIN so that I can award your coins to you.  You can whisper prize to me again once this is fixed.
+    put ooc $diapsid.winner [DR Discord Giveaways] Please use AVOID COIN so that I can award your coins to you.  You can whisper prize to me again once this is fixed.
     goto botWait
 
 
