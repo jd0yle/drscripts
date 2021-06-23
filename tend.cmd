1e0771bb21c607026b2d01c6ba9a1fdfd925a955
Justin Doyle <justin@jmdoyle.com>
2021-06-23 15:17:19 -0500

    Fixed tend script for mites

--- tend.cmd
+++ tend.cmd
@@ -8,15 +8,21 @@ action var parts %parts|$1 when blood mite on your (.*?)(?:,|\.)
 
 
 tend.top:
+    var parts none
 	gosub health
+	pause 1
 	var index 0
 	eval len count("%parts", "|")
 
+echo %parts
+
+if ("%parts" = "none") then goto done
 
 loop:
     echo thispart: %parts(%index)
-    if ("%parts(%index)" = "none") then goto done
-    gosub tend my %parts(%index)
+    if ("%parts(%index)" != "none") then {
+        gosub tend my %parts(%index)
+    }
     math index add 1
     if (%index > %len) then goto tend.top
     goto loop
