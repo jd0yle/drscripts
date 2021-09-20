81c2a95dc304f7ed3dcbcc4ac80ff636f5dfbc64
Justin Doyle <justin@jmdoyle.com>
2021-09-19 22:48:12 -0500

    Taisidon strings again again

--- taisidon.cmd
+++ taisidon.cmd
@@ -1,26 +1,30 @@
 include libmaster.cmd
 
-var suspects steward|artist|deckhand|chef|director|beautician|bartender|boatswain|entertainer
-var weapons comb|cleaver|corkscrew|knife|baton|logbook|zills|bottle|paintbrush
+var suspects artist|bartender|beautician|boatswain|chef|deckhand|director|entertainer|steward
+var weapons baton|cleaver|comb|corkscrew|bottle|knife|logbook|paintbrush|zills
 
-action var weapon comb when The.*slashes marked by odd perforations of the skin
+action var weapon baton when The.*soft tissue damage and internal bleeding
+action var weapon bottle when The.*severe lacerations
 action var weapon cleaver when The.*chop marks that reveal flesh and bone
+action var weapon comb when The.*slashes marked by odd perforations of the skin
 action var weapon corkscrew when The.* oddly curved puncture wounds
 action var weapon knife when The.*ragged edges
-action var weapon baton when The.*soft tissue damage and internal bleeding
-action var weapon zills when The.*clean edges
-action var weapon glass bottle when The.*severe lacerations
+action var weapon logbook when The.*severe blunt trauma
 action var weapon paintbrush when The.*deep and lethal puncture wounds
+action var weapon zills when The.*clean edges
+
 
 var murderer null
 action var murderer $1 when (\S+) says while
 action var murderer $1 when (\S+) says with
 action var murderer $1 when (\S+) says coughing
 action var murderer $1 when (\S+) says, fingers
-action var murderer $1 when (\S+) says\W
+action var murderer $1 when (\S+) says\s
+
 
 var location null
 action var location $roomname when A thorough search of the area uncovers an area of damp stickiness on the floor where a large amount of blood has partially dried
+
 
 put study corpse
 pause
@@ -44,12 +48,7 @@ move starboard
 move up
 gosub investigateRoom
 
-
-#echo location is %location
 eval location replacere("%location", "The Morada, ", "")
-#echo location is %location
-
-#echo put accuse %murderer with %weapon in %location
 put accuse %murderer with %weapon in %location
 
 exit
